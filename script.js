30f6df3c70152f0f22d6f4dd098d3af72cfac4bb
eshp9174-hub <eshp9174@gmail.com>
2026-05-20 22:59:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const options = document.querySelectorAll(".opt");
+
+  options.forEach(opt => {
+    opt.addEventListener("click", () => {
+      nextStep("step4");
+    });
+  });
+});
+
 function nextStep(id){
   document.querySelectorAll('.step').forEach(s => s.classList.remove('active'));
   document.getElementById(id).classList.add('active');
