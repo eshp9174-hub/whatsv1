7fe088e30c6936fd2315cbf27309bd0a776216b4
eshp9174-hub <eshp9174@gmail.com>
2026-05-20 22:57:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function nextStep(id) {
+function nextStep(id){
   document.querySelectorAll('.step').forEach(s => s.classList.remove('active'));
   document.getElementById(id).classList.add('active');
 }
